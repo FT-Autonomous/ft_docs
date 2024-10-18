c8f5a7bf9c1ebeccb48e459534f19718d4ed351c
Naza <uzoukwuc@tcd.ie>
2024-10-18 23:00:16 +0100

    Fix site bootstrapping tutorial
    This commit does a tonne of stuff:
    
    - Styles code blocks
    - Adds the 'image' and 'figure' tags for graphics
    - Adds the 'right' tag for managing right floats. I need a
      solution that allows me to align floating graphic with corresponding
      items in the main text body and this tag is just an ad hoc fix for
      that
    - Adds a page on making a good tutorial, more or less empty for now
    - Add some images to the site tutorial
    - Use layouts to avoid boilerplate

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -1,7 +1,30 @@
-import { defineMarkdocConfig } from '@astrojs/markdoc/config';
+import { defineMarkdocConfig, nodes, component } from '@astrojs/markdoc/config';
 import shiki from '@astrojs/markdoc/shiki';
 export default defineMarkdocConfig({
-    extends: [
-        shiki({ theme: 'one-dark-pro' }),
-    ],
+  tags: {
+    right: {
+      render: component('./src/components/Right.astro')
+    },
+    figure: {
+      attributes: {
+        ...nodes.image.attributes,
+        width: { type: String, required: false },
+        height: { type: String, required: false },
+        caption: { type: String, required: false }
+      },
+      render: component('./src/components/Figure.astro'),
+    },
+    image: {
+      attributes: {
+        ...nodes.image.attributes,
+        width: { type: String, required: false },
+        height: { type: String, required: false },
+        caption: { type: String, required: false }
+      },
+      render: component('./src/components/Image.astro'),
+    },
+  },
+  extends: [
+    shiki({ theme: 'one-dark-pro' }),
+  ],
 });
