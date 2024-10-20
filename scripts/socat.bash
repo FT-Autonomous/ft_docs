3a5f46d700cd076b7c71b7dc21fea02b176c1dfe
Naza <uzoukwuc@tcd.ie>
2024-10-20 02:40:48 +0100

    Allow precooked certs

--- scripts/socat.bash
+++ scripts/socat.bash
@@ -1,7 +1,14 @@
 #!/bin/bash
 . .env
-if mkcert $FT_DOCS_HOST
+
+CERT=./$FT_DOCS_HOST.pem
+KEY=$FT_DOCS_HOST-key.pem
+
+if [[ ! -e $CERT || ! -e $KEY ]]
 then
-    CERT=./$FT_DOCS_HOST.pem KEY=$FT_DOCS_HOST-key.pem
-    socat openssl-listen:7733,cert=$CERT,key=$KEY,verify=0,reuseaddr,fork tcp:localhost:8881
+    mkcert $FT_DOCS_HOST || exit 1
+else
+    echo Using existing certs
 fi
+
+socat openssl-listen:7733,cert=$CERT,key=$KEY,verify=0,reuseaddr,fork tcp:localhost:8881
