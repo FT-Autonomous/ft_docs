5f4addc438b7b2143a0daf245ae3b6e0d2d17f33
Naza <uzoukwuc@tcd.ie>
2025-01-19 23:41:20 +0000

    Color

--- src/theme.ts
+++ src/theme.ts
@@ -1,11 +1,7 @@
 /* Site Wide Color Schemes */
 export const backgroundColor = "#4B0000"; // Darker Red
 export const foregroundColor = "#FFFFFF"; // White
-export const secondaryBackgroundColor = "#4B0000"; // Red
-export const gradientStartColor = "#4B0000"; // Darker Red
-export const linkVisitedColor = "#4B0000"; // Darker Red
+export const secondaryBackgroundColor = "#200000"; // Red
+export const linkVisitedColor = "#FFAAAA"; // Darker Red
 export const linkHoverVisitedColor = "#4B0000"; // Darker Red
-export const linkHoverColor = "#B22222"; // Firebrick Red
-export const gradientStartEvenColor = "#4B0000"; // Darker Red
-export const gradientStartOddColor = "#4B0000"; // Darker Red
-export const gradientEndColor = "#4B0000"; // Darker Red
+export const linkHoverColor = "#B44444"; // Firebrick Red
